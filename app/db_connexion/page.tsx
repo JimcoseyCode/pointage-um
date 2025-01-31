9f4d2d5bbf5522e793014c3e41c818d1c8dfa85b
JimcoseyCode-dev <b.raphaelpro@gmail.com>
2025-01-31 15:05:36 +0100

    Ajout de la configuration initiale de Next.js avec Tailwind CSS et MongoDB, création de composants de base et mise en place de la structure du projet.

---
+++ app/db_connexion/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import { testDatabaseConnection } from "./actions";
+
+export default async function dbConnexion() {
+  const isConnected = await testDatabaseConnection();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col place-items-center gap-12">
+        <div className="relative flex place-items-center gap-6 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+          {" + "}
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] saturate-0 brightness-0 dark:saturate-100 dark:brightness-100"
+            src="/mongodb.svg"
+            alt="MongoDB Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+        {isConnected ? (
+          <h2 className="text-lg text-green-500">
+            Vous ètes connecter à MongoDB !
+          </h2>
+        ) : (
+          <h2 className="text-lg text-red-500">
+            Vous n'ètes pas connecter à MongoDB !
+          </h2>
+        )}
+      </div>
+    </main>
+  );
+}
---
+++ app/docs/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export default function Docs() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <figure className="mt-10">
+          <blockquote className="text-center text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
+            <p>
+              “Cette page est un référentiel complet de la documentation du
+              projet. Vous y trouverez une description détaillée des
+              fonctionnalités implémentées, ainsi que des informations
+              techniques et contextuelles.”
+            </p>
+          </blockquote>
+        </figure>
+        <div className="fixed bottom-0 left-0 flex w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="flex  gap-2 hover:underline hover:underline-offset-4 text-xl/8"
+            href="https://gitlab.com/JimcoseyCode/fds-pointage-um"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/file.svg"
+              alt="File icon"
+              width={20}
+              height={20}
+            />
+            Code source
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
