0422bb105bb03c3e57b1f4f93774addbd8cd424d
JimcoseyCode-dev <b.raphaelpro@gmail.com>
2025-03-18 13:54:08 +0100

    Revert "fix v2.0.1"
    This reverts commit a86e09c8514d8db54ddbeeb6b02454fad17aa06c.

--- app/auth/components/SignUpForm.tsx
+++ app/auth/components/SignUpForm.tsx
@@ -9,7 +9,7 @@ import {
     SelectItem,
     SelectTrigger,
     SelectValue,
-} from "./ui/select";
+} from "@/components/ui/select";
 import { FormData} from "../types";
 import { UserRole } from "@/types";
 
